aeb87606377537951119e79bb37fb4b73bd80eef
Poul Henriksen <POHE@zealand.dk>
2023-11-01 10:30:27 +0100

    fdff

---
+++ UML3_2023/MenuItemNumberExist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML3_2023
+{
+    public class MenuItemNumberExist:Exception
+    {
+        public MenuItemNumberExist(string message):base(message)
+        {
+            
+        }
+
+        public MenuItemNumberExist()
+        {
+            
+        }
+    }
+}
